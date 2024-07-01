abde8a6929f777abdbd60efd8bf5723d93fe6f8d
kouki928 <furukawavo@gmail.com>
2024-07-01 12:37:42 +0900

    for分

--- basic_grammar.go
+++ basic_grammar.go
@@ -6,29 +6,38 @@ import (
 )
 
 func main() {
-	for i := 0; i < 3; i++ {
-		fmt.Println("My favorite number is", rand.Intn(10))
-	}
-	j := 0
-	for j < 1 {
-		rnd := rand.Intn(10)
-		if rnd < 5 || rnd == 10 {
-			fmt.Println("While loop", rnd)
-			j += 1
-		}
-	}
-	item := [3]int{4, 5, 6}
-	for i := range item {
-		fmt.Println(item[i], i)
-	}
 
-	for i := range item {
-		fmt.Println(item[i], i)
-	}
+	rand_methods()
+
+	for_templates()
+
 }
 
 func rand_methods() {
 	// int型の 0 ~ n - 1 の値を生成
 	rnd_num := rand.Intn(10)
 	fmt.Println("Random Number is", rnd_num)
+}
+
+func for_templates() {
+	/* 馴染み深い for ------------------------------------------ */
+	for i := 0; i < 3; i++ {
+		fmt.Println(i)
+	}
+
+	/* While的な書き方 ----------------------------------------- */
+	j := 0
+	for j < 3 {
+		rnd := rand.Intn(10)
+		if rnd < 5 || rnd == 10 {
+			fmt.Println("While loop", rnd)
+			j += 1
+		}
+	}
+
+	/* 配列を見る --------------------------------------------- */
+	item := [3]int{4, 5, 6}
+	for i := range item {
+		fmt.Println(item[i], i)
+	}
 }
