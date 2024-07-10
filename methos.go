61e22dd9db6dcf5a5a93af4f74c579b6ccb548f7
kouki928 <furukawavo@gmail.com>
2024-07-10 23:47:17 +0900

    date convert

--- methos.go
+++ methos.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"html/template"
+	"log"
+	"strconv"
 	"time"
 )
 
@@ -62,8 +66,55 @@ func (d *Date) ConvertDateString(dateString string) {
 }
 
 func datetime_sample() {
-	var d_string string = "2024-07-02 15:00"
+	var d_string string = "2024-07-02 5:12"
 	var date Date
 	date.ConvertDateString(d_string)
 	fmt.Println(date)
+	var date2 Date2
+	date2.ConvertDateString(d_string)
+	fmt.Println(date2)
+}
+
+type Date2 struct {
+	dateTime   time.Time
+	dateString string
+}
+
+// Next.js から受け取った TimeStamp: string を Date型 にする
+// 受け取る値は yyyy-MM-dd hh:mm を想定
+// 引数の形をチェックするのは今回パス
+func (d *Date2) ConvertDateString(dateString string) {
+	layout := "2006-01-02 15:04"
+	parsedTime, err := time.Parse(layout, dateString)
+	if err != nil {
+		fmt.Println("InValid dateString")
+		return
+	}
+
+	d.dateTime = parsedTime
+
+	weekDays := [7]string{"日", "月", "火", "水", "木", "金", "土"}
+	var result bytes.Buffer
+	dateField := map[string]string{
+		"Year":    strconv.Itoa(parsedTime.Year()),
+		"Month":   strconv.Itoa(int(parsedTime.Month())),
+		"Day":     strconv.Itoa(parsedTime.Day()),
+		"WeekDay": weekDays[parsedTime.Weekday()],
+		"Hour":    strconv.Itoa(parsedTime.Hour()),
+		"Minute":  strconv.Itoa(parsedTime.Minute()),
+	}
+	for key, value := range dateField {
+		if key != "WeekDay" && len(value) < 2 {
+			dateField[key] = "0" + value
+		}
+	}
+	text, err := template.New("resultDate").Parse("{{.Year}}/{{.Month}}/{{.Day}} ({{.WeekDay}}) {{.Hour}}:{{.Minute}}")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = text.Execute(&result, dateField); err != nil {
+		log.Fatal(err)
+	} else {
+		d.dateString = result.String()
+	}
 }
